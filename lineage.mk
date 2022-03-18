8ca86841e5d0de9527c038dcf3ba5dfa882f128c
hlvs-apps <henri@sauers.de>
2022-03-18 19:22:01 +0100

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/kminilte/car.mk)
 
 # Inherit from kminilte device
-$(call inherit-product, device/samsung/hlvs-apps/device.mk)
+$(call inherit-product, device/samsung/kminilte/device.mk)
 
 
 $(call inherit-product, device/samsung/kminilte/full_kminilte.mk)
