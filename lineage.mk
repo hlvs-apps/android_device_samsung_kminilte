f2566af5ebd5dc69d2be85f38c99ded9f564dde4
hlvs-apps <henri@sauers.de>
2022-03-18 19:36:10 +0100

    Update

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 #Inspired by https://github.com/snappautomotive/firmware-device_samsung_gts4lvaaos/commit/af84d711eb917c2ea117afad20cea7d6a5353655#diff-d55a17587dffc6383f5e66d55fdd0c47ce4053502b5819892587952114fbc159
 
 # Inherit Snapp/hlvs-apps modifications
-$(call inherit-product, device/samsung/kminilte/additions.mk)
+$(call inherit-product, device/hlvs-apps/common/additions.mk)
 
 
 # Inherit some common Lineage stuff.
